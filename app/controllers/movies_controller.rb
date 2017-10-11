d044481a14913d044ada505abf8be545bb54d761
Michael <mbovi@uncc.edu>
2017-10-11 16:46:49 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,11 @@ def show
   # will render app/views/movies/show.html.haml by default
 end
 
+def new
+  @movie = Movie.new
+  # default: render 'new' template
+end 
+
 private
 
   def movie_params
